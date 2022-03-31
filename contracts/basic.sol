02e32acfb80adde55b65d0a854e582659703bda7
Sihan <sihantawsik@gmail.com>
2022-03-31 23:55:26 +0600

    update: Game factory contract

--- contracts/basic.sol
+++ contracts/basic.sol
@@ -4,8 +4,14 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts@4.4.0/token/ERC20/IERC20.sol";
 
 contract GameFactory {
-    function createbasicGame(string[] memory _hints, bytes32 _secretNumber, int _maxAttempts) public returns (BasicGame){
-        return new BasicGame(_hints, _secretNumber, _maxAttempts);
+    address token = 0xb454f9AbecB9f3feF62A446353353db8BDaC9AB0;
+    address reserve = 0xaaB762c4a13C188054C172ee001347EB80F5E6dC;
+
+    event GameCreated(address _address);
+
+    function createbasicGame(string[] memory _hints, bytes32 _secretNumber, int _maxAttempts) public {
+        BasicGame basicGame = new BasicGame(_hints, _secretNumber, _maxAttempts);
+        emit GameCreated(address(basicGame));
     }
 }
 
@@ -16,20 +22,11 @@ contract BasicGame {
     string[] private hints;
     address[] private winners;
     address owner;
-    address token;
-    address reserve;
 
     constructor(string[] memory _hints, bytes32 _secretNumber, int _maxAttempts) {
         owner = msg.sender;
         hints = _hints;
-        token = 0x1D0Ae71a8A5536D4D47eE057d986eEB872A8d2F4;
-        reserve = 0xaaB762c4a13C188054C172ee001347EB80F5E6dC;
         secretNumber = _secretNumber;
         maxAttemps = _maxAttempts;
-
-        // Approving token spending
-        IERC20(token).approve(reserve, 200 * 10**18);
-        IERC20(token).approve(address(this), 200 * 10**18);
-        IERC20(token).transferFrom(reserve, address(this), 200 * 10**18);
     }
 }
--- contracts/etk.sol
+++ contracts/etk.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract EdenToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Eden Token", "ETK") {
+contract EdenToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor() ERC20("Eden Token", "ETK") ERC20Permit("Eden Token") {
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 
