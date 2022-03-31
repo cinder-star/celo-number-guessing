291aa3b9e1d1f9020072afc1e3d5c9f6bfdd6220
Sihan <sihantawsik@gmail.com>
2022-03-31 21:41:54 +0600

    add: Game factory contract

--- contracts/basic.sol
+++ contracts/basic.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts@4.4.0/token/ERC20/IERC20.sol";
 
+contract GameFactory {
+    function createbasicGame(string[] memory _hints, bytes32 _secretNumber, int _maxAttempts) public returns (BasicGame){
+        return new BasicGame(_hints, _secretNumber, _maxAttempts);
+    }
+}
+
 
 contract BasicGame {
     bytes32 private secretNumber;
