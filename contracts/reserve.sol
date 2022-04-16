2db42f4d4809c256ec4b464fc4c5518cbba84863
Sihan <sihantawsik@gmail.com>
2022-04-17 01:13:10 +0600

    update: Game reserve only owner distribute

--- contracts/reserve.sol
+++ contracts/reserve.sol
@@ -6,8 +6,18 @@ import "@openzeppelin/contracts@4.4.0/token/ERC20/IERC20.sol";
 contract Reserve {
     address constant gameFactoryAddress = 0xDe21397d9F5cAf17dBF1B999ac612eF1EFfe1855;
     address constant token = 0xb454f9AbecB9f3feF62A446353353db8BDaC9AB0;
+    address private owner;
 
-    function distribute (address _address, uint _amount) external payable {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner allowed.");
+        _;
+    }
+
+    function distribute (address _address, uint _amount) external payable onlyOwner {
         address lastGame = GameFactory(gameFactoryAddress).getLastGame();
         if (BasicGame(lastGame).isPayablePlayer(_address)) {
             IERC20(token).transfer(_address, _amount);
